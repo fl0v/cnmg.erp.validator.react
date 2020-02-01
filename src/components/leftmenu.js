c1c6e8428435095e3238bf8039d4c100af90fbd1
fl0v <7046246+fl0v@users.noreply.github.com>
2020-02-01 15:39:31 +0200

    left menu icon

--- src/components/leftmenu.js
+++ src/components/leftmenu.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
+import { ReactComponent as Img } from "/src/res/menu.svg";
 
 function StorageMenuItem(props) {
-  const storage = props.storage  
-  return <li><a>{storage.name}</a></li>;
+  const storage = props.storage;
+  const [storageId, setStorageId] = useState(false);
+  const selectStorage = (el) => setStorageId(el.data.id)
+  return (
+    <li>
+      <a href="#{storage.id}" className="text-reset" onClick={selectStorage}>{storage.name}</a>
+    </li>
+  );
 }
 
-export function LeftMenu(props) {
+export function LeftMenu(props) {  
   const [expanded, setExpanded] = useState(false);
-  const items = props.storages.map(m => <StorageMenuItem storage={m} />);
+  const items = props.storages.map((m) => <StorageMenuItem storage={m} />);
   return (
-    <section id="left-menu" className={expanded ? 'expanded' : ''}>
-      <a href="#" onClick={() => setExpanded(!expanded)} className="icon" ></a>
-      <ul class="content">
-        {items}   
-      </ul>  
+    <section id="left-menu" className={expanded ? "expanded p-3" : "m-3"}>
+      <a href="#left-menu" onClick={() => setExpanded(!expanded)} className="icon">
+        <Img />
+      </a>
+      <ul class="content list-unstyled">{items}</ul>
     </section>
-  );
+  );  
 }
--- src/validator.js
+++ src/validator.js
@@ -30,24 +30,25 @@ function Header(props) {
   let storage = props.storage;
   return (
     <header>
-      <h1>{storage &&
-        <span>{storage.name}</span>
-      } {props.cinema.name}</h1>
+      <h1>
+        {storage && <span>{storage.name}</span>} {props.cinema.name}
+      </h1>
     </header>
   );
 }
 
 function CodeMessage(props) {
   const messages = props.messages;
-  const html = messages.map(m => <li dangerouslySetInnerHTML={{ __html: m }} />);
+  const html = messages.map(m => (
+    <li dangerouslySetInnerHTML={{ __html: m }} />
+  ));
   return <ul class="messages list-unstyled">{html}</ul>;
 }
 
-function CodeForm(props) {  
-
+function CodeForm(props) {
   const messages = ["<h2>mesaj 1</h2>", "<p>mesaj 2</p>"];
 
-  return (    
+  return (
     <form className="code m-3">
       <CodeMessage messages={messages} />
       <div className="form-group fixed-bottom m-3 mx-auto">
@@ -59,19 +60,18 @@ function CodeForm(props) {
         />
         <button type="submit" className="btn btn-primary w-100">
           Validate
-      </button>
+        </button>
       </div>
-      
-      </form>  
+    </form>
   );
 }
 
 export default function Validator() {
-  let storage = StorageList.find((storage) => storage.id == Settings.storageId)
+  let storage = StorageList.find(storage => storage.id === Settings.storageId);
   return (
-    <div id="validator">    
+    <div id="validator">
       <Header cinema={Cinema} storage={storage} />
-      <LeftMenu cinema={Cinema} storages={StorageList} />
+      <LeftMenu cinema={Cinema} storages={StorageList} settings={Settings} />
       <CodeForm />
     </div>
   );
